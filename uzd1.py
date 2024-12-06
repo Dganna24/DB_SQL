0350a662b6c99241d849a2ac35341d467798746d
Dganna24 <bandzu@icloud.com>
2024-12-06 23:20:21 +0200

    add new

---
+++ uzd1.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+from main import select_command
+print('1\n')
+select_command("""SELECT ASMENSKODAS, VARDAS, PAVARDE FROM DARBUOTOJAS
+WHERE GIMIMOMETAI = '1988-07-20';""")
+
+print('2\n')
+select_command("""SELECT * FROM DARBUOTOJAS
+WHERE GIMIMOMETAI < '1988-07-29';""")
+
+print('3\n')
+select_command("""SELECT VARDAS, PAVARDE, DIRBANUO, GIMIMOMETAI FROM DARBUOTOJAS
+WHERE DIRBANUO BETWEEN '2009-10-30' AND '2012-11-11';""")
+
+print('4\n')
+select_command("""SELECT VARDAS, PAVARDE, SKYRIUS_PAVADINIMAS, PROJEKTAS_ID FROM DARBUOTOJAS
+WHERE PROJEKTAS_ID IN (2, 3);""")
+
+print('5\n')
+select_command("""SELECT VARDAS, PAVARDE, ASMENSKODAS FROM DARBUOTOJAS
+WHERE ASMENSKODAS LIKE '4%';""")
+
+print('6\n')
+select_command("""SELECT vardas, strftime('%Y', GIMIMOMETAI) AS GIMIMOMETAI FROM DARBUOTOJAS WHERE GIMIMOMETAI LIKE '%-12';""")
+
+
+print('7\n')
+select_command("""SELECT * FROM PROJEKTAS WHERE PAVADINIMAS LIKE '__u%';""")
+
